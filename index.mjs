6d7631fab7fd28b08a4510aafdf4345c71295b64
PonomareVlad <Git@PonomareVlad.ru>
2020-04-06 17:53:14 +0500

    Base version

---
+++ index.mjs
@@ -0,0 +1,33 @@
+export default class ReplaceFromUTM {
+    constructor({
+                    initial = null,
+                    replacements = null,
+                    prefixes = {phone: 'tel:', email: 'mailto:'},
+                    source = new URLSearchParams(location.search).get('utm_source')
+                } = {}) {
+        Object.assign(this, {initial, replacements, prefixes, source});
+        ['initial', 'replacements'].forEach(key => {
+            if (!this[key]) throw `Empty replacement data: ${key}`;
+        });
+        document.addEventListener("DOMContentLoaded", this.initReplacement.bind(this));
+    }
+
+    initReplacement() {
+        const {initial, replacements, prefixes, source} = this;
+        if (!source || !replacements[source]) return `Source not handled: ${source}`;
+        const replacement = replacements[source];
+        Object.keys(replacement).forEach(field => {
+            if (!initial[field]) return `Field not initialized: ${field}`;
+            const prefix = (prefixes && prefixes[field]) ? prefixes[field] : '';
+            document.querySelectorAll(`a[href="${prefix + initial[field]}"]`)
+                .forEach(node => (node.href = prefix + replacement[field]) &&
+                    this.replaceTextNodeRecursive(node, initial[field], replacement[field]));
+        })
+        console.debug(this);
+    }
+
+    replaceTextNodeRecursive(node, search, replace) {
+        if (node.childElementCount) node.childNodes.forEach(node => this.replaceTextNodeRecursive(node, search, replace));
+        else if (node.textContent === search) node.textContent = replace;
+    }
+}
